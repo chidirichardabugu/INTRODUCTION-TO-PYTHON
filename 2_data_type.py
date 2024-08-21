24c0423ffbf24a8e68eedfe7574bfad808c410be
chidirichardabugu <techcoheirs@gmail.com>
2024-08-21 14:46:07 +0200

    creating a variable number

---
+++ 2_data_type.py
@@ -0,0 +1,16 @@
+num = 10
+num_type = type(num)
+print(num_type)
+print("num_type")
+
+name_1 = "Zainab"
+name_2 = "Blessing"
+name_3 = "muideen"
+name_4 = "Richard"
+name_5 = "Mr Richard"
+
+data_type_name = type(name_4)
+print(data_type_name)
+
+sola_age = (40)
+print(sola_age)

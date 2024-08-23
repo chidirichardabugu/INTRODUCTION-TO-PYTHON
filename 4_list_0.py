60eeb4c47cb04fb2703e5f422b1eda7c7d8c4ff0
chidirichardabugu <techcoheirs@gmail.com>
2024-08-23 12:05:57 +0200

    creating a variable name

--- 4_list_0.py
+++ 4_list_0.py
@@ -2,4 +2,15 @@ myTechlist = ["python","database","azure","reactjs"]
 # Appending html to myTechlist
 myTechlist.append("html")
 
+
+# print item on index 2
+print (myTechlist[2])
+
+
+# print the lenght of your list
+print(len(myTechlist))
+
+# insert AWS, Cybersecurity
+myTechlist.insert(5,"Cybersecurity")
+
 print(myTechlist)

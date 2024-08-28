a553921c3711dcd64813165d0faf336ad2db125a
chidirichardabugu <techcoheirs@gmail.com>
2024-08-28 12:32:07 +0200

    update

---
+++ 5_2_list.py
@@ -0,0 +1,11 @@
+def get_even_num(myNum):
+    evenNum = []
+    for num in myNum:
+        if myNum % 2 == 0:
+            evenNum.append(num)
+        return evenNum
+    
+original_list = [1, 2, 3, 4, 6, 7, 8]
+even_list = get_even_num(original_list)
+print("Original list:", original_list)
+print("even_list:", even_list)

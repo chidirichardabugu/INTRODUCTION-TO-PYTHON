fb6370489a6b347243fb6bc76d75dae02d7f26a1
chidirichardabugu <techcoheirs@gmail.com>
2024-09-02 12:12:55 +0200

    creating a variable name

---
+++ 20_dict.py
@@ -0,0 +1,7 @@
+# create a python dictionary with keys as the names of three countris & value as their capital.print the capitalof the second country
+countrise_and_capital = {
+    "Nigria": "Abuja",
+    "England": "London",
+    "Ghana": "Acara"
+}
+print(countrise_and_capital["England"])

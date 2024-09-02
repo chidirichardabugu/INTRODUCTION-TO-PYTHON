c7e0fca98e0f7f2f62cb2e1aee7d59214682bacd
chidirichardabugu <techcoheirs@gmail.com>
2024-09-02 12:28:31 +0200

    creating a variable name

--- 20_dict.py
+++ 20_dict.py
@@ -4,4 +4,10 @@ countrise_and_capital = {
     "England": "London",
     "Ghana": "Acara"
 }
+#method 1
 print(countrise_and_capital["England"])
+
+#method 2
+second_country = list(countrise_and_capital.keys())[1]
+capital_of_second_country = countrise_and_capital[second_country]
+print(capital_of_second_country)
